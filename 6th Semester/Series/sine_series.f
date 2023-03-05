37b26665d53a4eb87200826ca9dc3fada33396a2
MurshedSK <murshedsk135@gmail.com>
2023-03-05 12:51:35 +0530

    Series

--- 6th Semester/Series/sine_series.f
+++ 6th Semester/Series/sine_series.f
@@ -6,21 +6,21 @@
             do i = 1, 9 
                   read(11, 2)z
 2                 format(f6.1)
-                  x = z-int(z/360)*360
-                  x = acos(-1.0)*x/180.0
+                  x = z - int(z / 360) * 360
+                  x = acos(- 1.0) * x / 180.0
                   term = x
                   sum = x
                   test = 1e-6
                   n = 0
-                  do while(abs(term).gt.test)
-                        term = -term*(x**2)/((2*n+3)*(2*n+2))
+                  do while(abs(term) .gt. test)
+                        term = - term * (x**2) / ((2*n + 3)*(2 * n +2))
                         sum = sum + term
-                        n = n+1
+                        n = n + 1
                   end do
                   y = sin(x)
-                  diff = abs(sum-y)
+                  diff = abs(sum - y)
                   write(22,3)z, x, sum, y, diff
-3                 format(f6.1,3x,f7.3,3x,f8.5,1x,f8.5,1x,f8.5)                  
+3                 format(f6.1, 3x, f7.3, 3x, f8.5, 1x, f8.5, 1x, f8.5)                  
             end do
             stop
             end
